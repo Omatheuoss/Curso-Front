1d9c6500cbfb8df535d85891b9563e1c263f4d8e
Matheus Lima <matheuspatrocinio258@gmail.com>
2023-09-05 11:54:29 -0300

    novo repositorio

---
+++ html/atividade15/script.js
@@ -0,0 +1,18 @@
+
+function blue(){
+    const div = document.getElementById('quadrado')
+    div.style.backgroundColor = 'blue'  
+}
+function red(){
+    const div = document.getElementById('quadrado')
+    div.style.backgroundColor = 'red'
+}
+function soltou(){
+    const div = document.getElementById('quadrado')
+    div.innerText = 'soltou'
+}
+function clicou{
+    const div = document.getElementById('quadrado')
+    div.innerText = 'clicou'
+}
+
