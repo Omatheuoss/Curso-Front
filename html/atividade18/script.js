2759d8752b9ffe2f53f69b6f28241f5cdbf740cc
Matheus Lima <matheuspatrocinio258@gmail.com>
2023-09-11 11:39:57 -0300

    novo repositorio

---
+++ html/atividade18/script.js
@@ -0,0 +1,35 @@
+// const container = document.querySelector('.container')
+
+// const div = document.createElement('div')
+
+// div.classList.add('quadrado')
+
+// container.appendChild(div)
+
+// const botao = document.querySelector('button')
+
+// const container = document.querySelector('.container')
+
+// const div = document.createElement('div')
+
+// div.classList.add('circulo')
+
+// container.appendChild(div)
+
+// botao.addEventListener('click', ()=>{
+//     circulo.classList.toggle('hide')
+// }
+
+const itemlista = Array.from(document.querySelectorAll ('li'));
+
+// itemlista[2].innerText = 'Matheus'
+
+for(let i = 0; i < itemlista.length; i++){
+
+    itemlista[i].innerText = 'Aula top'
+}
+
+itemlista.map(elemnto)=>{
+    
+    elemento.innerText = 'Matheus'
+}
