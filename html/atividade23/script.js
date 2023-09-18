b77807f913a89974cf9e785e0b96efcfcf7f6cee
Matheus Lima <matheuspatrocinio258@gmail.com>
2023-09-18 11:46:25 -0300

    novo repositorio

---
+++ html/atividade23/script.js
@@ -0,0 +1,27 @@
+// const divLocal = document.getElementById('Session')
+// const divSession = document.getElementById('Local')
+
+// const nomes = ['lucian', 'Renata', 'Bruno', 'Matheu']
+
+// localStorage.setItem('teste', JSON.stringify (nomes))
+
+// const recuperandoDados = JSON.parse(localStorage.getItem('teste')
+
+// console.log(typeof(recuperandoDados))
+const frutas = ["Abacaxi", "morango", "kiwi", "goiaba+"]
+
+// frutas.push("uva")// adicionar no final
+// frutas.unshift("maça") // adiciona ao inicio
+// frutas.shift()// remove do inicio
+
+let posicaoArray = frutas.indexOf("kiwi")
+
+frutas.splice(posicaoArray, 1)
+
+console.log(frutas)
+
+var nome = "Matheus"
+console.log(nome)
+
+console.log(nome,tolowerCase())
+console.log(nome,tolowerCase())
