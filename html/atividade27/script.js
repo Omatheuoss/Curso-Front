6fe5e05e1100e812b61951dfcac6b612e5cc99fe
Matheus Lima <matheuspatrocinio258@gmail.com>
2023-09-28 08:43:22 -0300

    novo repositorio

--- html/atividade27/script.js
+++ html/atividade27/script.js
@@ -1,30 +1,18 @@
-// const KEY_OPENAI = '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'
+const container = document.querySelector('.container')
 
-// let mensagem = 'Quais são as capitais do Brasil?'
 
-// fetch("https://api.openai.com/v1/completions",{
-//     method:'POST',
-//     headers:{
-//         Accept: "application/json",
-//         "Content-type":  "application/json",
-//         Authorization: `Bearer ${KEY_OPENAI}`
-//     },
-//     body: JSON.stringify({
-//         model: "text-daVinci-003",
-//         prompt: mensagem, 
-//         max_tokens: 2048,
-//         temperatura: 0.5
-//     })
-    
-// })
-// .then(response => response.json())
-// .then(data => {
 
-// })
-// .catch(erro =>{
-//     console.log(errou)
-// })
 
 fetch('https://fakestoreapi.com/products?limit=5')
             .then(res=>res.json())
-            .then(json=>console.log(json))
+            .then(json=>{
+                console.log(json)
+
+            produto.forEach(produto => {
+                container.innerHTML += `<div class="card">
+                <img src="../../Imagens/pngwing.com (1).png" alt="Imagem do Card 1">
+                <h2>$(produto.tittle)</h2>
+                <p> Airsoft M4 rifle M16</p>
+            </div>`
+            });
+            })
